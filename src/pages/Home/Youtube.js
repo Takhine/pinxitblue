ec379c944238d6da18e70e5b5683ede3d8e0ddd6
Aniketh <anikethnair97@gmail.com>
2020-03-21 21:49:53 +0530

    Testing

--- src/pages/Home/Youtube.js
+++ src/pages/Home/Youtube.js
@@ -6,28 +6,6 @@ export default ({ youtubeId }) => {
 
     const [play, setPlay] = useState(false);
     return (
-        //   <div
-        //     className="video"
-        //     style={{
-        //       position: "relative",
-        //       paddingBottom: "56.25%" /* 16:9 */,
-        //       paddingTop: 25,
-        //       height: 0
-        //     }}
-        //   >
-        //     <iframe
-        //       style={{
-        //         position: "absolute",
-        //         top: 0,
-        //         left: 0,
-        //         width: "100%",
-        //         height: "100%"
-        //       }}
-        //       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-        //       src={`https://www.youtube.com/embed/${youtubeId}`}
-        //       frameBorder="0"
-        //     />
-        //   </div>
         <div
             className="video"
             style={{
